1f77cdf00052b59b49c93a8a411897e2ac08700c
chenchang <chenzhang@nonobank.com>
2016-10-20 15:18:12 +0800

    补充回调测试

--- src/main/java/com/bao/model/ResponseResult.java
+++ src/main/java/com/bao/model/ResponseResult.java
@@ -10,4 +10,28 @@ public class ResponseResult<T> {
     private String errorCode;
     private String errorMessage;
     private T data;
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
